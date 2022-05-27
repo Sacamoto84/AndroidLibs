5493f7d1f51dbee3b511b96e666e40936d4105fc
zorromen <zorromen@mail.ru>
2022-05-27 14:50:13 +0300

    V1

--- libs/thead.kt
+++ libs/thead.kt
@@ -1,18 +1,5 @@
 package libs
 
 /*
-* /////////////////////////////////////////////////////
-*Создание потока                                     //
-* /////////////////////////////////////////////////////
-* val threadWithRunnable = Thread(udp_DataArrival()) //
-* threadWithRunnable.start()                         //
-*                                                    //
-* class udp_DataArrival : Runnable {                 //
-*   override fun run() {                             //
-*        while (true) {                              //
-*          ...                                       //
-*        }                                           //
-*    }                                               //
-* }                                                  //
-* /////////////////////////////////////////////////////
+
 */
