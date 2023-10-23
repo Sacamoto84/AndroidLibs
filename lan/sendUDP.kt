f074f4b6524ffad1ff407f09c5a3da34066d7c7d
Sacamoto84 <zorromen@mail.ru>
2023-10-23 09:43:31 +0300

    V282.5

--- lan/sendUDP.kt
+++ lan/sendUDP.kt
@@ -11,25 +11,4 @@ import java.net.InetAddress
 
 
 
-//=====================================================
-// Отправить Udp сообщение * Возвращает OK или ошибку
-// region // sendUDP(messageStr: String, ip :String, port: Int): String
 
-fun sendUDP(messageStr: String, ip: String, port: Int): String {
-    // Hack Prevent crash (sending should be done using an async task)
-    val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-    StrictMode.setThreadPolicy(policy)
-    try {
-        val socket = DatagramSocket()
-        socket.broadcast = true
-        val sendData = messageStr.toByteArray()
-        val sendPacket =
-            DatagramPacket(sendData, sendData.size, InetAddress.getByName(ip), port)
-        socket.send(sendPacket)
-        println("sendUDP: $ip:$port")
-    } catch (e: IOException) {
-        Timber.e("IOException: " + e.message)
-        return e.message.toString()
-    }
-    return "OK"
-}
